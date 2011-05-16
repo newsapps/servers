cb4b646ff7e79ba854d492e3fb52667a7d9a230c
Ryan Mark <rmark@tribune.com>
2011-05-16 15:51:11 -0500

    added a new recipe for a wordpress cluster using RDS, bug fix in varnish_reload for ubuntu natty

---
+++ boot-scripts/wordpress/admin-nfs.sh
@@ -0,0 +1,29 @@
+{%extends 'wordpress/base.sh' %}
+
+{% block install %}
+# install some basic stuff
+DEBIAN_FRONTEND='noninteractive' \
+apt-get -q -y -o Dpkg::Options::='--force-confnew' install \
+        zip git-core subversion unattended-upgrades \
+        build-essential \
+        apache2 \
+        php5 php5-mysql php5-gd php-pear libapache2-mod-php5 php-apc php5-curl \
+        mysql-client \
+        postfix
+
+# enable mod_rewrite
+a2enmod rewrite
+
+# Install apache config
+cp /home/$USERNAME/cloud-commander/wordpress/etc/apache2/apache2.conf /etc/apache2/apache2.conf
+chown root:root /etc/apache2/apache2.conf
+chmod 644 /etc/apache2/apache2.conf
+
+# Needs a reboot -
+{% include "_nfs-server.sh" %}
+
+{% endblock %}
+
+{% block finish %}
+reboot
+{% endblock %}
