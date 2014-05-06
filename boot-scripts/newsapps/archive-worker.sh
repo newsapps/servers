72022cbb046f07294be1e0971cc6c627684b5452
Ryan Mark <ryan@mrk.cc>
2014-05-06 12:45:30 -0500

    make sure /mnt/tmp gets created

--- boot-scripts/newsapps/archive-worker.sh
+++ boot-scripts/newsapps/archive-worker.sh
@@ -4,4 +4,7 @@
 install_pkg libgraphicsmagick++1-dev libboost-python-dev imagemagick enchant \
   graphicsmagick python-pgmagick libtiff-dev liblcms2-dev libwebp-dev \
   python-imaging libboost-python-dev
+
+mkdir /mnt/tmp
+chmod ugo+rw /mnt/tmp
 {% endblock %}
